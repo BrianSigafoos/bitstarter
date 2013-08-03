c46753eec34617093989bdd7e1b3d5202b01cb54
Brian Sigafoos <bisigle@gmail.com>
2013-08-03 21:47:03 +0400

    * moved css and js to public/css directory * configured express to load static css / js in web.js * added "fork me on github" style top right link to give props to coursera * removed old hw files * removed shadows/shading towards more flat design

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var express = require('express');
+var port = process.env.PORT || 8080;
 
 var app = express.createServer(express.logger());
 
@@ -8,7 +9,22 @@ app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+// express is serving /public/css /public/js etc now
+// instead of loading from external urls
+// from https://github.com/spadin/simple-express-static-server/blob/master/server.js
+// commented out parts that I wasn't sure about their use
+app.configure(function(){
+//    app.use(express.methodOverride());
+//    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
+//    app.use(express.errorHandler({
+//        dumpExceptions: true,
+//        showStack: true
+//    }));
+//    app.use(app.router);
 });
+
+app.listen(port, function() {
+  console.log("Listening at localhost:" + port);
+});
+
