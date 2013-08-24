ae4b3dab2505d8d6bc2c773151b055831f8f2b2f
Brian Sigafoos <bisigle@gmail.com>
2013-08-24 19:09:09 +0400

    * setting up with stuff from HW7

---
+++ web-old.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var express = require('express');
+var port = process.env.PORT || 8080;
+
+var herokuappname = "photolearn"
+var domainname = "www.photolearnapp.com"
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var buf = fs.readFileSync('index.html');
+  if (request == "http://" + herokuappname + ".herokuapp.com"){response.redirect('http://' + domainname);}
+  else {response.send(buf.toString());}
+});
+
+// express is serving /public/css /public/js etc now
+// instead of loading from external urls
+// from https://github.com/spadin/simple-express-static-server/blob/master/server.js
+// commented out parts that I wasn't sure about their use
+app.configure(function(){
+//    app.use(express.methodOverride());
+//    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
+//    app.use(express.errorHandler({
+//        dumpExceptions: true,
+//        showStack: true
+//    }));
+//    app.use(app.router);
+});
+
+app.listen(port, function() {
+  console.log("Listening at localhost:" + port);
+});
+
